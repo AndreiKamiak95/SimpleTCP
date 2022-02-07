469f072fae0209875657cfaf172cecb9eca8aa7a
Andrei Kamiak <andrej.komyak@yandex.by>
2022-02-07 14:22:18 +0300

    First commit

---
+++ TcpClient/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace TcpClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(390, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(122, 42);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(284, 18);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 26);
+            this.txtPort.TabIndex = 9;
+            this.txtPort.Text = "8910";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(61, 19);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(173, 26);
+            this.txtHost.TabIndex = 8;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(240, 22);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(38, 20);
+            this.lblPort.TabIndex = 7;
+            this.lblPort.Text = "Port";
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(12, 21);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(43, 20);
+            this.lblHost.TabIndex = 6;
+            this.lblHost.Text = "Host";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(16, 70);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(496, 118);
+            this.txtMessage.TabIndex = 11;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(390, 194);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(122, 42);
+            this.btnSend.TabIndex = 12;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(16, 242);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(496, 118);
+            this.txtStatus.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
